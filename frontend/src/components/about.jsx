80a878e0c67ac9eb281203b2262446b40785b809
YehonatanHen <yonatan.h333@gmail.com>
2020-10-22 14:34:55 +0300

    init commit

---
+++ frontend/src/components/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+
+class About extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>About</h2>
+            </div>
+        );
+    }
+}
+
+export default About
