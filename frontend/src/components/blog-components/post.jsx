69cc6b2b2ffb998b4c661e921660b3f807e7a2dc
YehonatanHen <yonatan.h333@gmail.com>
2020-10-26 17:49:44 +0200

    can see only one post each time

--- frontend/src/components/blog-components/post.jsx
+++ frontend/src/components/blog-components/post.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Card, Accordion, Button } from 'react-bootstrap';
 
+var key = 0
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props)
+        this.key = 0
+    }
+
     render() {
         return (
             <>
             <Card>
             <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                <Accordion.Toggle as={Button} variant="link" eventKey={(++key).toString()}>
                     Click me!
                 </Accordion.Toggle>
             </Card.Header>
-                <Accordion.Collapse eventKey="0">
+                <Accordion.Collapse eventKey={(key).toString()}>
                     <Card.Body>Hello! I'm the body</Card.Body>
                 </Accordion.Collapse>
             </Card>
