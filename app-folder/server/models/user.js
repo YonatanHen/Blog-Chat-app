a069945e2e506271b3f34dfddc71db2af2a5837e
YehonatanHen <yonatan.h333@gmail.com>
2020-10-29 14:41:24 +0200

    Figure how to manage find user error, in prrogress

--- app-folder/server/models/user.js
+++ app-folder/server/models/user.js
@@ -5,28 +5,31 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
-        trim: true
+        // required: true,
+        // trim: true
     },
     email: {
         type: String,
-        required: true,
-        lowercase: true,
-        trim: true
+        // required: true,
+        // lowercase: true,
+        // trim: true
     },
     password: {
         type: String,
-        required: true
-    }
-})
+        // required: true
+    },
+},
+{ typeKey: '$type' }
+)
 
 userSchema.statics.findUser = async (username) => {
     const user = await User.findOne({ username })
 
     if (!user) {
+        console.log(username)
         return new Error('Unable to find user:' + username)
     }
-
+    console.log(user)
     return user
 }
 
