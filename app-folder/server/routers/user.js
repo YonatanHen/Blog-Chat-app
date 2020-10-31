6f4b77c9a2ea8c298367ffa4c6373263c342ebbd
YehonatanHen <yonatan.h333@gmail.com>
2020-10-31 16:00:17 +0200

    progress with signin. still, there is an error

--- app-folder/server/routers/user.js
+++ app-folder/server/routers/user.js
@@ -2,17 +2,20 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
+
 router.get('/login/:username', async (req, res) => {
     const user = await User.findUser( req.params.username )
     res.send(user)
 })
 
-router.post('/signin/', async (err,req,res,next) => {
+router.post('/signin', async (req,res) => {
     try {
-        await req.user.save()
+        console.log(req.body.params)
+        
+        await req.body.save()
         res.send()
     } catch (e) {
-        req.status(500).send()
+        res.status(500).send()
     }
 })
 
