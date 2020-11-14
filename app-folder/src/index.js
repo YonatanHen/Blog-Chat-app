ac0ca5ff085f75a7881db01517887ff9b8cd1a40
YehonatanHen <yonatan.h333@gmail.com>
2020-11-14 21:16:31 +0200

    delete unecssary prints and imports

--- app-folder/src/index.js
+++ app-folder/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app'
-import { CookiesProvider } from "react-cookie";
 
 
-
-ReactDOM.render(
-    <CookiesProvider>
-        <App />
-    </CookiesProvider>,
-    document.getElementById("root"));
+ReactDOM.render(<App />,document.getElementById("root"));
