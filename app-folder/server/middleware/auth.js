1fbbfe134b815710a96f2c5b4d3918a37e1fda5f
YehonatanHen <yonatan.h333@gmail.com>
2021-02-13 15:11:02 +0200

    design + other references updated

--- app-folder/server/middleware/auth.js
+++ app-folder/server/middleware/auth.js
@@ -14,7 +14,7 @@ const auth = async (req,res,next) => {
         req.user = user
         next()
     } catch (e) {
-        res.status(401).send({ error: 'You need to authentice!'})
+        res.status(401).send({ error: 'You need to authenticate!'})
     }
 }
 
