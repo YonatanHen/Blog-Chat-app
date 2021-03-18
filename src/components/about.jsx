ac9912877aa982862b78691c160691aec3b59675
YehonatanHen <yonatan.h333@gmail.com>
2021-03-18 15:14:09 +0200

    gitignore working

---
+++ src/components/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './navbar'
+
+
+class About extends React.Component {
+    render(){
+        return (
+            <div>
+                <Navbar/>
+                <h2>About</h2>
+            </div>
+        );
+    }
+}
+
+export default About
---
+++ src/components/chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './navbar'
+
+
+class Chat extends React.Component {
+    render(){
+        return (
+            <>
+                <Navbar/>
+                <h2>Chat</h2>
+            </>
+        );
+    }
+}
+
+export default Chat
