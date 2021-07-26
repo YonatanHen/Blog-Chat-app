c291feddbfa5b5c429558fb4ec57b1466c479d94
YehonatanHen <yonatan.h333@gmail.com>
2021-07-27 01:25:01 +0300

    log-in page rewritten with hooks, redux initialized

--- server/models/user.js
+++ server/models/user.js
@@ -2,60 +2,80 @@ require('../db/mongoose')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const Post = require('./post')
-
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        trim: true
-    }
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+		trim: true,
+	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				require: true,
+			},
+		},
+	],
 })
 
 userSchema.virtual('tasks', {
-    ref: 'Task',
-    localField: '_id', //the field that save the local connection stored data
-    foreignField: 'owner' //the field in the other side of the association that connects between them.
+	ref: 'Task',
+	localField: '_id', //the field that save the local connection stored data
+	foreignField: 'owner', //the field in the other side of the association that connects between them.
 })
 
-userSchema.statics.findByUsernameAndPassword = async (username,password) => {
-    const user = await User.findOne({ username })
-    if (!user) {
-        throw new Error('Unable to find user:' + username)
-    }
+userSchema.methods.generateAuthToken = async function () {
+	const user = this //access to this is more easier with this line
+	const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+		expiresIn: '1 days',
+	})
 
-    const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) {
-        throw new Error('Unable to login')
-    }
-    return user
+	user.tokens = user.tokens.concat({ token })
+	await user.save()
+
+	return token
+}
+
+userSchema.statics.findByUsernameAndPassword = async (username, password) => {
+	const user = await User.findOne({ username })
+	if (!user) {
+		throw new Error('Unable to find user:' + username)
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password)
+	if (!isMatch) {
+		throw new Error('Unable to login')
+	}
+	return user
 }
 
 userSchema.pre('save', async function (next) {
-    const user = this
+	const user = this
 
-    if(user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8)
+	}
 
-    next()
+	next()
 })
 
-userSchema.pre('remove', async function(next) {
-    const user = this
-    await Post.deleteMany({author: user._id})
+userSchema.pre('remove', async function (next) {
+	const user = this
+	await Post.deleteMany({ author: user._id })
 
-    next()
+	next()
 })
 
 const User = mongoose.model('User', userSchema)
