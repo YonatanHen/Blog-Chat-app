99565893a0f374f3c1d1a3940370948b5300d2ab
YehonatanHen <yonatan.h333@gmail.com>
2022-01-27 23:43:52 +0200

    chat added

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Form, InputGroup, FormControl, Button } from 'react-bootstrap'
+import '../css/chat.css'
+import {
+	Form,
+	InputGroup,
+	FormControl,
+	Button,
+	Container,
+} from 'react-bootstrap'
 import socketIOClient from 'socket.io-client'
 import moment from 'moment'
 import { useSelector } from 'react-redux'
+import { Prompt } from 'react-router-dom'
 
 const socket = socketIOClient()
+
+let key = 0
 
 const Chat = () => {
 	const user = useSelector((state) => state.user)
@@ -13,37 +23,65 @@ const Chat = () => {
 
 	const sendMessageHandler = (event) => {
 		event.preventDefault()
-		socket.emit('sendMessage', message, (error) => {
+		socket.emit(
+			'sendMessage',
+			{ message: message, user: user.username },
+			(error) => {
+				if (error) {
+					alert(error)
+				} else {
+					console.log('Message delivered!')
+				}
+			}
+		)
+
+		setMessage('')
+	}
+
+	useEffect(() => {
+		socket.emit('join', { user: user.username }, (error) => {
 			if (error) {
 				alert(error)
 			} else {
 				console.log('Message delivered!')
 			}
 		})
-
-		setMessage('')
-	}
+	}, [])
 
 	useEffect(() => {
 		socket.on('message', (message) => {
-			console.log(message)
 			setMessages(messages.concat(message))
 		})
 	}, [messages])
 
+	useEffect(() => {
+		socket.emit('disconnect', { user: user.username }, (error) => {
+			if (error) {
+				alert(error)
+			} else {
+				console.log('Message delivered!')
+			}
+		})
+	})
+
 	return (
-		<>
-			<h2>Chat</h2>
-			<div id='messages'>
+		<div className='chat'>
+			<div className='messages'>
 				{messages.map((msg) => {
+					key++
 					return (
-						<div key={msg}>
-							<p>{moment(msg.createdAt).format('h:mm A')} - {msg.text}</p>
+						<div key={key} className='message-box'>
+							{console.log(msg)}
+							<h6>
+								{msg.user ? `${msg.user} -` : ''}{' '}
+								{moment(msg.createdAt).format('HH:mm A')}
+							</h6>
+							<p> {msg.text}</p>
 						</div>
 					)
 				})}
 			</div>
-			<Form onSubmit={sendMessageHandler}>
+			<Form onSubmit={sendMessageHandler} className='message-input'>
 				<InputGroup>
 					<FormControl
 						placeholder='Type your message...'
@@ -55,7 +93,7 @@ const Chat = () => {
 					</Button>
 				</InputGroup>
 			</Form>
-		</>
+		</div>
 	)
 }
 
