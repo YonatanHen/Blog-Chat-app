54cb865566a803d18dac7a9d893163f529cb030d
Yonatan Hen <yonatan.h333@gmail.com>
2026-07-22 20:58:54 +0300

    chore(client): remove stray build artifacts committed in the scaffold

--- apps/client/src/main.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx("h1", { children: "Blog-Chat" }) }));
